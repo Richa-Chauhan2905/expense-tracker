08e1e0356a7c06518f46b763f3d973d576dc8f3c
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-07-18 00:18:24 +0530

    logout route added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { login, signup, updateCurrency } from '../controller/user.controller.js'
+import { login, logout, signup, updateCurrency } from '../controller/user.controller.js'
 import { protectRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.post("/signup", signup)
 router.post("/login", login)
+router.post("/logout", logout)
 router.patch("/currency", protectRoute, updateCurrency)
 
 export default router
